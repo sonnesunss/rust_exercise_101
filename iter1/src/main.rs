abc8c44651b31228ad556c82d088438de387395b
sun <145575806+sonnesunss@users.noreply.github.com>
2025-04-17 18:47:48 +0800

    into_iter
    获得迭代器所有权， 指的是迭代器获得集合元素的所有权

--- iter1/src/main.rs
+++ iter1/src/main.rs
@@ -1,6 +1,7 @@
 // 迭代器
 fn main() {
     let v1 = vec![1, 2, 5, 3, 8, 5, 31, 89, 432, 32798, 3809, 937, 74];
+    // iter()获得v1的不可变引用迭代器
     let _ = v1.iter().for_each(|ele| {
         println!("{}", ele);
     });
@@ -10,5 +11,10 @@ fn main() {
     println!("v1 {:?}", v1);
     println!("v2 {:?}", v2);
 
+    // into_iter()获得v2的所有权迭代器，迭代器可以获得v2所有元素的所有权
+    // 之后再通过v2访问元素rust静态编译器不会编译通过
+    let v3: Vec<_> = v2.into_iter().filter(|ele| ele % 2 == 0).collect();
+
+    println!("v3: {:?}", v3);
     println!("Hello, world!");
 }
