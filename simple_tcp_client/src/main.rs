2954cc8370fcac00f3ddfce8309399b5df345354
sun <145575806+sonnesunss@users.noreply.github.com>
2025-04-18 15:53:01 +0800

    更新客户端读取逻辑
    使用vec向量类型处理来自服务器的写入信息，好处是可以动态缩减，无须实现预测写入信息的大小，可能造成数组分配很大控件造成浪费，可能很小不足以一次处理完毕造成丢失或者需要添加额外的处理逻辑
    
    使用vec + read_to_end方法实现

--- simple_tcp_client/src/main.rs
+++ simple_tcp_client/src/main.rs
@@ -4,9 +4,9 @@ use std::net::TcpStream;
 fn main() -> std::io::Result<()> {
     println!("client started");
     let mut client = TcpStream::connect("127.0.0.1:9090").expect("Could't connect this server");
-    let mut read_buffer: [u8; 1024] = [0; 1024];
+    let mut read_buffer: Vec<u8> = Vec::new();
 
-    let r = client.read(&mut read_buffer)?;
+    let r = client.read_to_end(&mut read_buffer)?;
     println!("{:?}", r);
 
     let s = String::from_utf8_lossy(&read_buffer);
