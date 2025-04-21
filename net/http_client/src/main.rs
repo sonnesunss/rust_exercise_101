fadb81e46e9a006c053ac9b0a57566220b6ce78e
sun <145575806+sonnesunss@users.noreply.github.com>
2025-04-21 18:57:53 +0800

    发起一个简单的http请求
    使用reqwest、tokio

---
+++ net/http_client/src/main.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    println!("{resp:#?}");
+    Ok(())
+}
