e4ae25768a7491718a0fa17b7c27839ed1de8c5a
sun <145575806+sonnesunss@users.noreply.github.com>
2025-04-25 16:01:10 +0800

    Update main.rs
    添加了返回对非实现Copy trait类型的max_value支持，并且不获取所有权

--- syntax_basic/generic2/src/main.rs
+++ syntax_basic/generic2/src/main.rs
@@ -5,6 +5,13 @@ fn main() {
     let _r2 = my_add(1.1, 2.2);
     let _r3 = max_value(4, 321);
     let _r4 = max_value(5.324, 5668.321);
+
+    let s1 = "haha".to_string();
+    let s2 = "hahahahah".to_string();
+
+    let _rs1 = max_value2(&s1, &s2);
+
+    println!("{}", _rs1);
 }
 
 // 泛型函数
@@ -16,6 +23,15 @@ fn my_add<T: std::ops::Add<Output = T>>(one: T, two: T) -> T {
 #[allow(dead_code)]
 fn max_value<T: PartialOrd + PartialEq>(one: T, two: T) -> T {
     if std::cmp::PartialOrd::gt(&one, &two) {
+        return one;
+    }
+
+    two
+}
+
+#[allow(dead_code)]
+fn max_value2<'a, T: Ord>(one: &'a T, two: &'a T) -> &'a T {
+    if std::cmp::PartialOrd::gt(one, two) {
         return one;
     }
 
