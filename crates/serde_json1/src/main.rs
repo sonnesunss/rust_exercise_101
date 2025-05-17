ef572faab042ebe5ec4b38cd689a31301ff11b3b
sun <145575806+sonnesunss@users.noreply.github.com>
2025-05-18 07:15:45 +0800

    Update main.rs
    1.  tagged enum
    
    2. untagged
    
    3. original enum

--- crates/serde_json1/src/main.rs
+++ crates/serde_json1/src/main.rs
@@ -18,7 +18,49 @@ struct Y(i32);
 #[derive(Debug, Clone, Serialize)]
 struct Z;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Params {
+    p1: String,
+    p2: i32,
+    p3: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Value {
+    n: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Message {
+    Request {
+        id: String,
+        method: String,
+        params: Params,
+    },
+    Response {
+        id: String,
+        result: Value,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "root_type")]
+enum Message2 {
+    Request { id: String, method: String },
+
+    Response { id: String, result: Value },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Message3 {
+    Request { id: String, method: String },
+
+    Response { id: String, result: Value },
+}
+
 fn main() {
+    // struct
     let w1 = W { a: 0, b: 0 };
     let x1 = X(1, 2);
     let y1 = Y(3);
@@ -29,8 +71,33 @@ fn main() {
     let r3 = serde_json::to_string(&y1).unwrap();
     let r4 = serde_json::to_string(&z1).unwrap();
 
-    println!("{:?}", r1);
-    println!("{:?}", r2);
-    println!("{:?}", r3);
-    println!("{:?}", r4);
+    // enum
+    let msg1 = Message::Response {
+        id: "101".to_string(),
+        result: Value {
+            n: "sun".to_string(),
+        },
+    };
+
+    let msg2 = Message2::Request {
+        id: "67889".to_string(),
+        method: "get".to_string(),
+    };
+
+    let msg3 = Message3::Request {
+        id: "102".to_string(),
+        method: "post".to_string(),
+    };
+
+    let r5 = serde_json::to_string(&msg1).unwrap();
+    let r6 = serde_json::to_string(&msg2).unwrap();
+    let r7 = serde_json::to_string(&msg3).unwrap();
+
+    println!("c-like struct Serialized -> {:?}", r1);
+    println!("struct-tuple Serialized  -> {:?}", r2);
+    println!("struct-tuple Serialized  -> {:?}", r3);
+    println!("struct-empty Serialized  -> {:?}", r4);
+    println!("tagged enum Serialized   ->{:?}", r5);
+    println!("tagged enum Serialized   ->{:?}", r6);
+    println!("untagged enum Serialized ->{:?}", r7);
 }
