3c3c3c3ee565ea7f9c021e4e19af4cb45751f3a5
sun <145575806+sonnesunss@users.noreply.github.com>
2025-05-21 12:10:42 +0800

    添加json 反序列化的支持

--- crates/reqwest_post/src/main.rs
+++ crates/reqwest_post/src/main.rs
@@ -1,5 +1,6 @@
 use pretty_printer::pretty_print;
 use reqwest::{self, Client};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,12 +15,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     println!("response is -> {:?}", response.status());
-
-    let response_body = response.text().await?;
-
-    println!("response body iis -> {:?}", response_body);
+    let rb = response.json::<OAuthToken>().await?;
+    println!("response body is -> {:?}", rb);
+    println!("response body is ---> {:?}", rb.access_token);
+    println!("response body is ---> {:?}", rb.refresh_token);
 
     pretty_print!(json_data);
 
     Ok(())
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Serialize, Deserialize)]
+struct OAuthToken {
+    access_token: String,
+    refresh_token: String,
 }
