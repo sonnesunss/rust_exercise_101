c1733c93a1c47eb44022e5749b0468753cc6a2bd
sun <145575806+sonnesunss@users.noreply.github.com>
2025-05-22 08:01:50 +0800

    mini_redis
    实现get、set命令

--- crates/my_redis/examples/main.rs
+++ crates/my_redis/examples/main.rs
@@ -3,9 +3,9 @@ use mini_redis::{Result, client};
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
+    client.set("hello123", "world123".into()).await?;
 
-    let result = client.get("hello").await?;
+    let result = client.get("hello123").await?;
     println!("got value from the server; result={:?}", result);
 
     Ok(())
--- crates/my_redis/src/main.rs
+++ crates/my_redis/src/main.rs
@@ -18,15 +18,39 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // a HashMap iis used to store data
+    let mut db = HashMap::new();
+
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    // 持续从接收缓冲区读取frame
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        // 从读取到的frame解析出具体的命令
+        let response = match Command::from_frame(frame).unwrap() {
+            // 如果是set命令，就从中取出k-v并存储之
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            // 如果是get命令，则取出k，然后从db中根据key取出value，组合成Frame::Bluk返回
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+            // 未实现的命令
+            cmd => panic!("unimplemented! {:?}", cmd),
+        };
+
+        // 写回frame到底层的TcpStream中
         connection.write_frame(&response).await.unwrap();
     }
 }
